44777aa7dbb72fbad23cf182098681fd6633b0f1
ChadiNehme <150426661+ChadiNehme@users.noreply.github.com>
2025-03-24 22:32:21 +0200

    design admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,45 @@
+import { useContext } from "react";
 import Login from "./pages/Login"
-
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from "./context/AdminContext";
+import Navbar from "./component/Navbar";
+import SideBar from "./component/SideBar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddCoach from "./pages/Admin/AddCoach";
+import CoachList from "./pages/Admin/CoachList";
 
 function App() {
 
+  const { aToken } = useContext(AdminContext)
 
-  return (
-    <div>
-      <Login />
+
+
+  return !aToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointment" element={<AllAppointments />} />
+          <Route path="/add-coach" element={<AddCoach />} />
+          <Route path="/coach-list" element={<CoachList />} />
+
+
+
+        </Routes>
+      </div>
+
     </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   )
 }
 
